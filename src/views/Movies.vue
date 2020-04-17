a865e4d1a48c9794cfef0850ef015321a9f8da01
Mhmod-hsn <hsn.mhmod.96@gmail.com>
2020-04-17 21:55:15 +0200

    add fav items

--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -42,8 +42,8 @@
 						<v-card-actions>
 							<v-spacer></v-spacer>
 
-							<v-btn icon>
-								<v-icon>mdi-heart</v-icon>
+							<v-btn @click="addToFav(movie,$event)" icon>
+								<div :is-active="movie.fav" class="heart"></div>
 							</v-btn>
 
 							<v-btn icon>
@@ -84,16 +84,64 @@
         apiKey: 'fd5b76318bc43e4aec9f492e27f9bf16',
         movies: [],
         currentPage: 1,
-        buttonLoading: false
+        buttonLoading: false,
+        favouriteMovies: [],
+        myStorage: window.localStorage
       }
 	  },
     methods: {
+      addToFav(movie, event) {
+        // add to fav
+        if (!movie.fav) {
+          movie.fav = true;
+          let x = JSON.parse(JSON.stringify(movie));
+          this.favouriteMovies.push(x)
+        }
+        // // remove from favorite
+        else {
+          movie.fav = false;
+
+          // finding the current movie and removing from favourite
+          this.favouriteMovies = this.favouriteMovies.filter(e => {
+            return e.id !== movie.id
+          })
+        }
+
+        console.log('fav movie ', JSON.parse(JSON.stringify(this.favouriteMovies)));
+
+        this.myStorage.setItem('favourite-movies', JSON.stringify(this.favouriteMovies));
+
+        console.log(JSON.parse(this.myStorage.getItem('favourite-movies')))
+
+
+      },
       loadMore() {
         this.buttonLoading = true;
         axios(`https://api.themoviedb.org/3/discover/movie?api_key=${this.apiKey}&page=${this.currentPage}`)
           .then(response => {
             this.buttonLoading = false;
-            this.movies.push(...response.data.results);
+            let res = response.data.results;
+
+            // add heart icon if the movie was added before to favourite
+            if (this.favouriteMovies.length > 0) {
+              for (let m = 0; m < res.length; m++) {
+                res[m].fav = false;
+
+                for (let f = 0; f < this.favouriteMovies.length; f++) {
+                  if (res[m].id === this.favouriteMovies[f].id) {
+                    res[m].fav = true
+                  }
+                }
+                this.movies.push(res[m])
+              }
+
+            } else {
+              res.forEach(m => {
+                m.fav = false;
+                this.movies.push(m)
+              })
+            }
+
             this.currentPage++
           })
           .catch(err => {
@@ -103,11 +151,18 @@
     },
     mounted () {
       this.loadMore();
+      // Infinite Scrolling calculation
       window.onscroll = () => {
         let bottmOfWindow = document.documentElement.scrollTop +
           window.innerHeight === document.documentElement.offsetHeight;
 
         bottmOfWindow ? this.loadMore() : ''
+      };
+
+      // get fav movies from local storage
+      if (localStorage.getItem('favourite-movies').length) {
+        this.favouriteMovies =
+          JSON.parse(localStorage.getItem('favourite-movies'))
       }
 
     }
